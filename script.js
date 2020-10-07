a71d883f3ff3738e2549429c4d69feefdaf46739
sweetbabyjay <jayfly64@gmail.com>
2020-10-07 16:42:55 -0700

    add items for local storage and change of color for text hour when it is within that hour

--- script.js
+++ script.js
@@ -6,4 +6,54 @@ $(document).ready(function(){
 
     let now = parseInt(moment().format("HH"));
     console.log(now)
-})
+
+    let $9amText = $("#9amText");
+    let $10amText = $("#10amText");
+    let $11amText = $("#11amText");
+    let $12pmText = $("#12pmText");
+    let $1pmText = $("#1pmText");
+    let $2pmText = $("#2pmText");
+    let $3pmText = $("#3pmText");
+    let $4pmText = $("#4pmText");
+    let $5pmText = $("#5pmText");
+
+    $("textarea").each(function (){
+        let name = parseInt($(this).attr("name"));
+        if (name < now){
+            $(this).addClass("past");
+        }
+
+        if (name === now){
+            $(this).addClass("present");
+        }
+
+        if (name > now){
+            $(this).addClass("future");
+        }
+    })
+
+    $("button").on("click", function(){
+
+        localStorage.setItem("9AM", ($9amText()))
+        localStorage.setItem("10AM", ($10amText())) 
+        localStorage.setItem("11AM", ($11amText()))
+        localStorage.setItem("12PM", ($12pmText()))
+        localStorage.setItem("13PM", ($1pmText()))
+        localStorage.setItem("14PM", ($2pmText()))
+        localStorage.setItem("15PM", ($3pmText()))
+        localStorage.setItem("16PM", ($4pmText()))
+        localStorage.setItem("17PM", ($5pmText()))
+
+    })
+    
+    $("#9amText").append(localStorage.getItem("9AM"));
+    $("#10amText").append(localStorage.getItem("10AM"));
+    $("#11amText").append(localStorage.getItem("11AM"));
+    $("#12pmText").append(localStorage.getItem("12PM"));
+    $("#1PmText").append(localStorage.getItem("13PM"));
+    $("#2pmText").append(localStorage.getItem("14PM"));
+    $("#3pmText").append(localStorage.getItem("15PM"));
+    $("#4pmText").append(localStorage.getItem("16PM"));
+    $("#5pmText").append(localStorage.getItem("17PM"));
+
+}) 
